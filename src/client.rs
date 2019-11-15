20fb4f7c85ab6068497d95125ad77f750e3463ef
ccyanxyz <790266922@qq.com>
2019-11-16 01:40:42 +0800

    add verify to client

--- src/client.rs
+++ src/client.rs
@@ -7,10 +7,13 @@ use std::net::TcpStream;
 use std::str::from_utf8;
 use std::vec::Vec;
 use std::fs::File;
-use std::fs::OpenOptions;
 
 use argparse::{ ArgumentParser, StoreTrue, Store };
 use stopwatch::{ Stopwatch };
+
+extern crate rustc_serialize;
+use rustc_serialize::hex::ToHex;
+extern crate hex;
 
 mod eddsa;
 use eddsa::*;
@@ -26,12 +29,21 @@ fn main() {
     let mut keyfile = "client.key".to_string();
     let mut sign = false;
     let mut verify = false;
+    let mut r = "".to_string();
+    let mut s = "".to_string();
+    let mut pubkey = "".to_string();
     let mut msg = "hello world".to_string();
     {
         let mut ap = ArgumentParser::new();
         ap.set_description("two party eddsa client");
-        ap.refer(&mut verbose)
-            .add_option(&["-v", "--verbose"], StoreTrue, "verbose");
+        ap.refer(&mut verify)
+            .add_option(&["-v", "--verify"], StoreTrue, "verify");
+        ap.refer(&mut r)
+            .add_option(&["--sig-r"], Store, "R");
+        ap.refer(&mut s)
+            .add_option(&["--sig-s"], Store, "s");
+        ap.refer(&mut pubkey)
+            .add_option(&["--pubkey"], Store, "aggrageted pubkey");
         ap.refer(&mut keygen)
             .add_option(&["-g", "--keygen"], StoreTrue, "keygen");
         ap.refer(&mut keyfile)
@@ -52,7 +64,12 @@ fn main() {
     }
     if sign {
         let sw = Stopwatch::start_new();
-        fn_sign(msg, &keyfile);
+        fn_sign(&msg, &keyfile);
+        println!("elapsed_time: {} ms", sw.elapsed_ms());
+    }
+    if verify {
+        let sw = Stopwatch::start_new();
+        fn_verify(&msg, &r, &s, &pubkey);
         println!("elapsed_time: {} ms", sw.elapsed_ms());
     }
     if net_delay {
@@ -66,7 +83,7 @@ fn fn_keygen(keyfile: &String) {
     let mut stream = TcpStream::connect(host).unwrap();
     let client_keypair: KeyPair = KeyPair::create();
 
-    println!("{:?}", client_keypair);
+    //println!("{:?}", client_keypair);
 
     let mut buf = vec![1u8];
     let mut client_pubkey = client_keypair.public_key.get_element().to_bytes().to_vec();
@@ -80,30 +97,30 @@ fn fn_keygen(keyfile: &String) {
     let eight: FE = ECScalar::from(&BigInt::from(8));
     let eight_inverse: FE = eight.invert();
     let server_pubkey = server_pubkey * &eight_inverse;
-    println!("server_pubkey: {:?}", server_pubkey);
+    //println!("server_pubkey: {:?}", server_pubkey);
 
     // calc aggregated pubkey
     let mut pks: Vec<GE> = Vec::new();
     pks.push(server_pubkey.clone());
     pks.push(client_keypair.public_key.clone());
     let key_agg = KeyPair::key_aggregation_n(&pks, &1);
-    println!("aggregated_pubkey: {:?}", key_agg);
+    println!("aggregated_pubkey: {:?}", key_agg.apk.get_element().to_bytes().to_hex());
 
     save_keyfile(keyfile, client_keypair, key_agg);
 }
 
-fn fn_sign(msg: String, keyfile: &String) {
+fn fn_sign(msg: &str, keyfile: &String) {
     let (client_keypair, key_agg) = load_keyfile(keyfile);
 
-    println!("client_keypair: {:?}", client_keypair);
-    println!("key_agg: {:?}", key_agg);
+    //println!("client_keypair: {:?}", client_keypair);
+    //println!("key_agg: {:?}", key_agg);
 
     let mut stream = TcpStream::connect(host).unwrap();
     // round 1
-    let msg = str_to_bigint(msg);
-    println!("msg: {:?}", msg);
+    let msg = str_to_bigint(msg.to_string());
+    //println!("msg: {:?}", msg);
     let (client_ephemeral_key, client_sign_first_msg, client_sign_second_msg) = Signature::create_ephemeral_key_and_commit(&client_keypair, BigInt::to_vec(&msg).as_slice());
-    println!("client_sign_first_msg: {:?}", client_sign_first_msg);
+    //println!("client_sign_first_msg: {:?}", client_sign_first_msg);
 
     let mut buf = vec![2u8];
     buf.append(&mut Converter::to_vec(&client_sign_first_msg.commitment));
@@ -115,10 +132,10 @@ fn fn_sign(msg: String, keyfile: &String) {
     let server_sign_first_msg = SignFirstMsg {
         commitment: BigInt::from(&buf[0..32]),
     };
-    println!("server_sign_first_msg: {:?}", server_sign_first_msg);
+    //println!("server_sign_first_msg: {:?}", server_sign_first_msg);
 
     // round 2
-    println!("client_sign_second_msg: {:?}", client_sign_second_msg);
+    //println!("client_sign_second_msg: {:?}", client_sign_second_msg);
     let mut buf: Vec<u8> = Vec::new(); 
     buf.append(&mut client_sign_second_msg.R.get_element().to_bytes().to_vec());
     buf.append(&mut Converter::to_vec(&client_sign_second_msg.blind_factor));
@@ -174,10 +191,41 @@ fn fn_sign(msg: String, keyfile: &String) {
     s.push(s2);
     let sig = Signature::add_signature_parts(s);
 
-    println!("sig: {:?}", sig);
+    println!("R: {:?}", sig.R.get_element().to_bytes().to_hex());
+    println!("s: {:?}", sig.s.get_element().to_bytes().to_hex());
 
     // verify
-    verify(&sig, BigInt::to_vec(&msg).as_slice(), &key_agg.apk);
+    //verify(&sig, BigInt::to_vec(&msg).as_slice(), &key_agg.apk);
+}
+
+fn fn_verify(msg: &str, r: &str, s: &str, pubkey: &str) {
+    let msg = str_to_bigint(msg.to_string());
+    let r_decode = hex::decode(r).expect("decode r failed");
+    let mut s_decode = hex::decode(s).expect("decode s failed");
+    s_decode.reverse();
+    let pubkey_decode = hex::decode(pubkey).expect("decode pubkey failed");
+
+    let eight: FE = ECScalar::from(&BigInt::from(8));
+    let eight_inverse: FE = eight.invert();
+
+    let r = GE::from_bytes(&r_decode).unwrap();
+    let r = r * &eight_inverse;
+    let s: FE = ECScalar::from(&BigInt::from(&s_decode[0..32]));
+    let pubkey = GE::from_bytes(&pubkey_decode).unwrap();
+    let pubkey = pubkey * &eight_inverse;
+    let sig = Signature {
+        R: r,
+        s: s,
+    };
+
+    match verify(&sig, BigInt::to_vec(&msg).as_slice(), &pubkey) {
+        Ok(_) => {
+            println!("true");
+        },
+        Err(err) => {
+            println!("false: {:?}", err);
+        }
+    }
 }
 
 fn fn_delay() {
@@ -196,6 +244,5 @@ fn str_to_bigint(msg: String) -> BigInt {
         .map(|b| format!("{:02X}", b))
         .collect();
     let msg = strs.join("");
-    println!("msg_hex: {:?}", msg);
     BigInt::from_hex(&msg)
 }
